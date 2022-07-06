a2384b6cbdc0554656b26e5824c782565ec8c958
inverseAgentSmith <theonewhocodes@gmail.com>
2022-07-06 12:58:10 +0200

    Initial Commit

---
+++ Stormchaser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stormchaser
+//
+//  Created by neo on 2022/07/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stormchaser/StormchaserApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormchaserApp.swift
+//  Stormchaser
+//
+//  Created by neo on 2022/07/06.
+//
+
+import SwiftUI
+
+@main
+struct StormchaserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
