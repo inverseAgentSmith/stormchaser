9d893ffae8908bfa862649e8bf8be2b7a9a765fa
inverseAgentSmith <theonewhocodes@gmail.com>
2022-07-07 03:06:28 +0200

    added city weather VStack

--- Stormchaser/ContentView.swift
+++ Stormchaser/ContentView.swift
@@ -10,8 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .gray, .orange], startPoint: .topLeading , endPoint: .bottomTrailing)
+            LinearGradient(colors: [.blue, .gray, .orange],
+                           startPoint: .topLeading ,
+                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Cape Town, WC")
+                    .font(.system(size: 32,
+                                  weight: .medium,
+                                  design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack (spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("26°")
+                        .foregroundColor(.white)
+                        .font(.system(size: 70, weight: .light, design: .default))
+                }
+                Spacer()
+                
+            }
         }
     }
 }
