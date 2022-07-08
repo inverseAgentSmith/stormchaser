1b1603571a41dabd7319570601c720faeeb76917
inverseAgentSmith <theonewhocodes10@gmail.com>
2022-07-08 12:09:15 +0200

    changed city view VStack to refactored CityWeatherView struct

--- Stormchaser/ContentView.swift
+++ Stormchaser/ContentView.swift
@@ -10,64 +10,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .gray, .orange],
-                           startPoint: .topLeading ,
-                           endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            BackgroundView(topColor: .blue, middleColor: .gray, bottomColor: .orange)
             
             VStack {
-                Text("Cape Town, WC")
-                    .font(.system(size: 32,
-                                  weight: .medium,
-                                  design: .default))
-                    .foregroundColor(.white)
-                    .padding(.top, 20)
-            
                 
-                VStack (spacing: 10) {
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
+                CityWeatherView(cityName: "Cape Town, WC",
+                                weatherImageName: "cloud.sun.fill",
+                                temperature: 26)
+                Spacer()
+                HStack(spacing: 30) {
+                    WeatherDayView(dayOfWeek: "TUE", weatherImageName: "sun.max.fill", temperature: 21)
                     
-                    Text("26°")
-                        .foregroundColor(.white)
-                        .font(.system(size: 70,
-                                      weight: .light,
+                    WeatherDayView(dayOfWeek: "WED", weatherImageName: "cloud.sun.fill", temperature: 21)
+                    
+                    WeatherDayView(dayOfWeek: "THU", weatherImageName: "cloud.fill", temperature: 21)
+                    
+                    WeatherDayView(dayOfWeek: "FRI", weatherImageName: "cloud.rain.fill", temperature: 21)
+                    
+                    WeatherDayView(dayOfWeek: "SAT", weatherImageName: "cloud.heavyrain.fill", temperature: 21)
+                }
+                Spacer()
+                
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(.white)
+                        .font(.system(size: 20,
+                                      weight: .semibold,
                                       design: .default))
-                    
-                    HStack(spacing: 20) {
-                        WeatherDayView(dayOfWeek: "TUE", weatherImage: "sun.max.fill", temperature: 21)
-                        
-                        WeatherDayView(dayOfWeek: "WED", weatherImage: "cloud.sun.fill", temperature: 21)
-                        
-                        WeatherDayView(dayOfWeek: "THU", weatherImage: "cloud.fill", temperature: 21)
-                        
-                        WeatherDayView(dayOfWeek: "FRI", weatherImage: "cloud.rain.fill", temperature: 21)
-                        
-                        WeatherDayView(dayOfWeek: "SAT", weatherImage: "cloud.heavyrain.fill", temperature: 21)
-                    }
-                    Spacer()
-                    
-                    Button {
-                        print("tapped")
-                    } label: {
-                        Text("Change Day Time")
-                            .frame(width: 280, height: 50)
-                            .background(.white)
-                            .font(.system(size: 20,
-                                          weight: .semibold,
-                                          design: .default))
-                            .foregroundColor(.blue)
-                            .cornerRadius(10)
-                    }
-                    Spacer()
+                        .foregroundColor(.blue)
+                        .cornerRadius(10)
                 }
             }
         }
     }
 }
+            
+            
     
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -78,7 +59,7 @@ struct ContentView_Previews: PreviewProvider {
 struct WeatherDayView: View {
     
     var dayOfWeek: String
-    var weatherImage: String
+    var weatherImageName: String
     var temperature: Int
     
     var body: some View {
@@ -88,7 +69,7 @@ struct WeatherDayView: View {
                 .font(.system(size: 16,
                               weight: .medium,
                               design: .default))
-            Image(systemName: weatherImage)
+            Image(systemName: weatherImageName)
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
@@ -100,6 +81,50 @@ struct WeatherDayView: View {
                               design: .default))
                 .foregroundColor(.white)
             
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var middleColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor,
+                                                   middleColor, bottomColor]),
+                       startPoint: .topLeading ,
+                       endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityWeatherView: View {
+    
+    var cityName: String
+    var weatherImageName: String
+    var temperature: Int
+    var body: some View {
+        VStack(spacing: 10) {
+            Text(cityName)
+                .font(.system(size: 32,
+                              weight: .medium,
+                              design: .default))
+                .foregroundColor(.white)
+                .padding(.top, 20)
+            
+            Image(systemName: weatherImageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .foregroundColor(.white)
+                .font(.system(size: 70,
+                              weight: .light,
+                              design: .default))
         }
     }
 }
